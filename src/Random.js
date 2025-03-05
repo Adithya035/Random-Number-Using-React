a2e653c3b35801abd88cf53e1f83b3af108e71fe
Adithya035 <adithya035@gmail.com>
2025-03-05 10:42:40 +0530

    Code Modified Accordingly

--- src/Random.js
+++ src/Random.js
@@ -2,20 +2,29 @@ import { useState } from "react"
 
 function Random() {
 
-    var [random,setrandom] = useState(0)
+    var [random, setrandom] = useState(0)
 
     function randomnumber() {
 
-        setrandom(Math.floor(Math.random()*10)+1)
+        setrandom(Math.floor(Math.random() * 10) + 1)
     }
 
-    return(
+    function reset() {
+
+        var reset__but = 0
+        setrandom(reset__but)
+    }
+
+    return (
 
         <div class="container">
 
             <h1 class="random__heading">Random Number Using React</h1>
             <h1 class="random__heading--modifier">{random}</h1>
-            <button onClick={randomnumber} class="button__random">Random Number</button>
+            <div class="button__container">
+                <button onClick={randomnumber} class="button__random">Random Number</button>
+                <button onClick={reset} class="button__reset">Reset</button>
+            </div>
         </div>
     )
 }
